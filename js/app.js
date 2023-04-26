a9878ac37918d4ec0638343e6db1c5fecf6bd831
pspauster <patrick.spauster@gmail.com>
2023-04-26 00:15:23 -0400

    added hover rules for all points improved popups
    also added count to main text and date to popup text

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 // a map of all motor vehicle crashes where people were killed in the past year
+
+//grab data from open data portal
 $.ajax({
     url: "https://data.cityofnewyork.us/resource/h9gi-nx95.geojson",
     type: "GET",
@@ -9,16 +11,15 @@ $.ajax({
         "$where": "number_of_pedestrians_killed > 0 AND crash_date > '2023-01-01T00:00:00.000'"
     }
 }).done(function (data) {
-    // alert("Retrieved " + data.length + " records from the dataset!");
-    console.log(data);
-    console.log(data.features)
-    console.log(data.features[0].geometry.coordinates[0])
-    console.log(data.features[0].properties.number_of_persons_killed)
-    console.log(data.features[0].properties)
 
     mapboxgl.accessToken = 'pk.eyJ1IjoicHNwYXVzdGVyIiwiYSI6ImNsZ2JxN3p4djAyZDkzZ3BmOGR1ZTVhcWQifQ.499a8nIbNZgnjPf0qIGx8g';
     const NYC_COORDINATES = [-74.00214, 40.71882]
 
+    //add a running total to the main text
+    var count = data.features.length
+    document.getElementById('count').innerHTML = count;
+
+    //set up map
     const map = new mapboxgl.Map({
         container: 'map', // container ID
         // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
@@ -29,15 +30,13 @@ $.ajax({
         pitch: 0
     });
 
-
+    //add sources and layers
     map.on('load', function () {
-
-        console.log(map.getStyle().layers)
-
 
         map.addSource('my-points', {
             type: 'geojson',
-            data: data
+            data: data,
+            generateId: true
         })
 
         map.addLayer({
@@ -46,6 +45,25 @@ $.ajax({
             source: 'my-points',
             paint: {
                 'circle-opacity': 0.6,
+                'circle-stroke-width': 1,
+                'circle-stroke-color': [
+                    'case',
+                    ['boolean', ['feature-state', 'hover'], false],
+                    "white", //add white outline on hover
+                    ['match', //otherwise outline is the same as circle color
+                        ['get', 'contributing_factor_vehicle_1'],
+                        'Unsafe Speed',
+                        'red',
+                        'Driver Inattention/Distraction',
+                        'purple',
+                        'Driver Inexperience',
+                        'green',
+                        'Traffic Control Disregarded',
+                        'gold',
+                        'Oversized Vehicle',
+                        'blue',
+                    /*other*/ '#000000']
+                ],
                 'circle-color': [
                     'match',
                     ['get', 'contributing_factor_vehicle_1'],
@@ -102,6 +120,7 @@ $.ajax({
 
     })
 
+    //create popups and conditional sentence for each point
     map.on('click', 'circle-my-points', (e) => {
 
         // conditional logic for sentence
@@ -116,17 +135,42 @@ $.ajax({
         //popups
         new mapboxgl.Popup()
             .setLngLat(e.lngLat)
-            .setHTML(`${e.features[0].properties.number_of_persons_killed} ${singPlural} ${verb} killed by a ${e.features[0].properties.vehicle_type_code1.toLowerCase()} due to ${e.features[0].properties.contributing_factor_vehicle_1.toLowerCase()} at ${e.features[0].properties.crash_time} hours on ${e.features[0].properties.crash_date.substring(0,10)}`)
+            .setHTML(`${e.features[0].properties.number_of_persons_killed} ${singPlural} ${verb} killed by a ${e.features[0].properties.vehicle_type_code1.toLowerCase()} due to ${e.features[0].properties.contributing_factor_vehicle_1.toLowerCase()} at ${e.features[0].properties.crash_time} hours on ${e.features[0].properties.crash_date.substring(0, 10)}`)
             .addTo(map)
     });
 
-    map.on('mousemove', 'circle-my-points', (event) => {
-        map.getCanvas().style.cursor = 'pointer';
-    })
 
-    map.on('mouseleave', 'circle-my-points', (e) => {
-        map.GetCanvas().style.cursor - ''
-    })
+    let hoveredStateId = null
+
+    // update featurestate when the mouse moves around within the points layer
+    map.on('mousemove', 'circle-my-points', (e) => {
+        if (e.features.length > 0) {
+            if (hoveredStateId !== null) {
+                map.setFeatureState(
+                    { source: 'my-points', id: hoveredStateId },
+                    { hover: false }
+                );
+                map.getCanvas().style.cursor = 'pointer';
+            }
+            hoveredStateId = e.features[0].id;
+            map.setFeatureState(
+                { source: 'my-points', id: hoveredStateId },
+                { hover: true }
+            );
+        }
+    });
+
+    // when the mouse leaves the points layer, make sure nothing has the hover featurestate
+    map.on('mouseleave', 'circle-my-points', () => {
+        if (hoveredStateId !== null) {
+            map.setFeatureState(
+                { source: 'my-points', id: hoveredStateId },
+                { hover: false }
+            );
+            map.getCanvas().style.cursor = '';
+        }
+        hoveredStateId = null;
+    });
 
 });
 
